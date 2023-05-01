2b62e90df26acc54cbcb5d2332387fbe898650b8
DaisyDzeV <123832658+DaisyDzeV@users.noreply.github.com>
2023-05-01 15:16:11 -0700

    Homework 4

---
+++ script.js
@@ -0,0 +1,10 @@
+let btn1 = document.querySelector('#btn-1');
+let btn2 = document.querySelector('#btn-2');
+
+
+btn1.addEventListener('click', () =>{
+	document.body.style.backgroundImage = "url('hw4_IMGs/lightblue.jpg')";
+});
+btn2.addEventListener('click', () =>{
+	document.body.style.backgroundImage = "url('hw4_IMGs/lightgold.jpg')";
+});

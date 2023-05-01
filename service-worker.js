403d6b74a4bdc669fe248735d6c1f577a5f62e97
DaisyDzeV <123832658+DaisyDzeV@users.noreply.github.com>
2023-04-30 18:28:14 -0700

    Homework 4
    Files for Homework 4 for CIS 128. Daisy V.

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+var CACHE_VERSION = 'myapp-v1';
+var CACHE_FILES = [
+    'Icons/iOS/Icon-48.png',
+	'Icons/iOS/Icon-100.png',
+	'Icons/iOS/Icon-196.png',
+    'app.js',
+    'styles.css'
+];
+
+self.addEventListener('install', event => {
+    console.log('SW installed');
+    event.waitUntil(
+        caches
+        .open(CACHE_VERSION)
+        .then(cache => {
+            console.log('SW caching files');
+            cache.addAll(CACHE_FILES)
+        })
+        .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', event => {
+    console.log('SW activated');
+    event.waitUntil(
+        caches.keys().then(keyNames => {
+            return Promise.all(
+                keyNames.map(key => {
+                    if(key !== CACHE_VERSION) {
+                        console.log('SW clearing old caches');
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    console.log('SW fetching');
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
+});
